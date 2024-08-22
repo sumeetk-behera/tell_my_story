ef77cf477b3866121e4dce6aeb0a6ac967087566
shashwat <07shashwat@gmail.com>
2024-08-22 13:01:54 +0530

    Added column names and added fields of AccountDetailsDto

--- src/main/java/com/example/tell_my_story/dto/AccountDetailDto.java
+++ src/main/java/com/example/tell_my_story/dto/AccountDetailDto.java
@@ -1,5 +1,21 @@
 package com.example.tell_my_story.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDetailDto {
 
+	private int id;
+
+	private String name;
+
+	private String email;
+
+	private String password;
+
+	private int age;
 }
