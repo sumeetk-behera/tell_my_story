cd890dec63b30b5801d3c53290640d344b0a6e7d
keerthip29 <keerthip2903@gmail.com>
2024-08-22 16:04:12 +0530

    CategoryDto updated

--- src/main/java/com/example/tell_my_story/dto/CategoryDto.java
+++ src/main/java/com/example/tell_my_story/dto/CategoryDto.java
@@ -1,5 +1,18 @@
 package com.example.tell_my_story.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDto {
 
+	
+	private int id;
+	@NotBlank(message="Category name should not be blank")
+	private String name;
 }
+
