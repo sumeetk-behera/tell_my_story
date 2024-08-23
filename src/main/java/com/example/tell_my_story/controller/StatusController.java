93195b04bfa2628d44ba2d4e63fd88329459abdf
shashwat <07shashwat@gmail.com>
2024-08-23 11:17:05 +0530

    get Status Api created

--- src/main/java/com/example/tell_my_story/controller/StatusController.java
+++ src/main/java/com/example/tell_my_story/controller/StatusController.java
@@ -2,6 +2,7 @@ package com.example.tell_my_story.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,10 @@ public class StatusController {
 		StatusDto status = statusService.addStatus(statusDto);
 		return ResponseEntity.ok(new ResponseDto(false, Constant.STATUS_ADDED, status));
 
+	}
+
+	@GetMapping("/getStatus")
+	public ResponseEntity<ResponseDto> getStatus() {
+		return ResponseEntity.ok(new ResponseDto(false, Constant.STATUS_ADDED, statusService.getStatus()));
 	}
 }
